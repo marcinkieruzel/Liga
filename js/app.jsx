5f41fb69c1053615227af7b9bf07116875299b47
kazikkoduje <kazik@kazikkoduje.pl>
2019-04-06 15:27:55 +0200

    done

--- js/app.jsx
+++ js/app.jsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import axios from 'axios';
 
 class Liga extends React.Component {
-   constructor(props) {
-   super(props);
-   this.state = {
-     teams: [],
-     games: [],
-     home: '',
-     guest: '',
-     homeScore: 0,
-     guestScore: 0
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: [],
+      games: [],
+      home: '',
+      guest: '',
+      homeScore: 0,
+      guestScore: 0
+    }
 
-   }
+  }
+
+  handlePoints = () => {
+    //Liczę punkty
+    let scores = this.state.games.map((e, i) => {
+      return {
+        home: e.home,
+        homePoints: e.homeScore > e.guestScore
+          ? 3
+          : (
+            e.homeScore == e.guestScore
+            ? 1
+            : 0),
+        guest: e.guest,
+        guestPoints: e.guestScore > e.homeScore
+          ? 3
+          : (
+            e.guestScore == e.homeScore
+            ? 1
+            : 0)
+      }
+    });
+
+    let points = [];
+
+    scores.forEach((e, i) => {
+
+      console.log(e.homePoints, e.guestPoints);
+      if (!points.find((val) => val.team === e.home)) {
+        points.push({team: e.home, score: e.homePoints})
+      } else {
+        let t = points.find((val) => val.team == e.home)
+        t.score = t.score + e.homePoints;
+      }
+      if (!points.find((val) => val.team === e.guest)) {
+        points.push({team: e.guest, score: e.guestPoints})
+      } else {
+        let t = points.find((val) => val.team == e.guest)
+        t.score = t.score + e.guestPoints;
+      }
+    })
+    return _.sortBy(points, (val) => val.score).reverse();
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -23,26 +67,66 @@ class Liga extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    axios.post('http://localhost:3000/games', {
+      //data: JSON.stringify({
+      //id: 12,
+      home: this.state.home,
+      guest: this.state.guest,
+      homeScore: this.state.homeScore,
+      guestScore: this.state.guestScore
+      //})
+    }).then(() => {
+      axios.get('http://localhost:3000/games').then((res) => {
+        this.setState({games: res.data})
+      })
+    })
+
   }
 
+  render() {
 
-   render(){
-     return (
-      <form onSubmit={this.handleSubmit}>
-        <input onChange={this.handleChange} value={this.state.home} type="text" name="home" placeholder="Gospodarz" />
-        <input onChange={this.handleChange} value={this.state.homeScore} type="text" name="homeScore" placeholder="Bramki Gospodarza" />
-        <input onChange={this.handleChange} value={this.state.guest} type="text" name="guest" placeholder="Gość" />
-        <input onChange={this.handleChange} value={this.state.guestScore} type="text" name="guestScore" placeholder="Bramki Gości" />
-        <button>Dodaj</button>
-      </form>
+    let table = this.handlePoints().map((e, i) => {
+      return (<tr key={i}>
+        <td>{e.team}</td>
+        <td>{e.score}</td>
+      </tr>)
+    })
 
-     )
-   }
- }
+    let games = this.state.games.map((e, i) => {
+      return <li key={i}>{e.home}
+        {e.homeScore}
+        : {e.guestScore}
+        {e.guest}</li>
+    })
 
-document.addEventListener('DOMContentLoaded', function(){
-    ReactDOM.render(
-        <Liga />,
-        document.getElementById('app')
-    );
+    return (<> < form onSubmit = {
+      this.handleSubmit
+    } > <input onChange={this.handleChange} value={this.state.home} type="text" name="home" placeholder="Gospodarz"/>
+    <input onChange={this.handleChange} value={this.state.homeScore} type="text" name="homeScore" placeholder="Bramki Gospodarza"/>
+    <input onChange={this.handleChange} value={this.state.guest} type="text" name="guest" placeholder="Gość"/>
+    <input onChange={this.handleChange} value={this.state.guestScore} type="text" name="guestScore" placeholder="Bramki Gości"/>
+    <button>Dodaj</button>
+  </form>
+  <div>
+    <ul>
+      {games}
+    </ul>
+    <table>
+      <tbody>
+        {table}
+      </tbody>
+    </table>
+  </div> < />)
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/games ').then((res) => {
+ this.setState({games: res.data})
+  })
+}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+ReactDOM.render(<Liga/>, document.getElementById('app'));
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 ### Tabela Ekstraklasy
 
 Na stronie renderuje się formularz służacy do dodawania meczów piłki nożnej.
-W pliku `db/games.json` znajduje się baza danych ligi. Uruchom baze przy pomocu json server `json server --watch ./db/games.json`
+W pliku `db/games.json` znajduje się baza danych ligi. Uruchom baze przy pomocu json server `json-server --watch ./db/games.json`
 
 Twoim zadaniem jest pokazanie listy meczów na stronie. Mecze pobierz z bazy danych przy pomocy fetch lub axios.
 
